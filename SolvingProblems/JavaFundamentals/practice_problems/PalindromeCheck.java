9e8b200daaf063e71e21945393fbca15b1c23e02
Prem404 <ppremkkumar010@gmail.com>
2026-08-22 15:17:24 +0530

    Practice Problem of Java Fundamentals

---
+++ SolvingProblems/JavaFundamentals/practice_problems/PalindromeCheck.java
@@ -0,0 +1,27 @@
+package JavaFundamentals.practice_problems;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        int origNumber = number;
+        int reversedNumber = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number = number / 10;
+        }
+
+        if (origNumber == reversedNumber) {
+            System.out.println(origNumber + " is a Palindrome.");
+        } else {
+            System.out.println(origNumber + " is not a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
