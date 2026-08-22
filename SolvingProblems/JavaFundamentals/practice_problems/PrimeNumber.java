da45686d22ce15743a69d2a51d52a627cdbc2798
Prem404 <ppremkkumar010@gmail.com>
2026-08-22 16:12:03 +0530

    modified PrimeNumber

--- SolvingProblems/JavaFundamentals/practice_problems/PrimeNumber.java
+++ SolvingProblems/JavaFundamentals/practice_problems/PrimeNumber.java
@@ -9,16 +9,13 @@ public class PrimeNumber {
 
         boolean isPrime = true;
 
-        if (number <= 1) {
-            isPrime = false;
-        } else {
-            for (int i = 2; i < number; i++) {
-                if (number % i == 0) {
-                    isPrime = false;
-                    break;
-                }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
             }
         }
+
 
         if (isPrime) {
             System.out.println(number + " is a Prime number.");
